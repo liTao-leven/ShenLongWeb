4b6f87d04ba6dd41f9429050a8cecea4c81b52f1
李涛 <litaoleven@126.com>
2022-02-24 15:58:11 +0800

    限制场景移出屏幕

---
+++ src/settings.6d159.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","hero","well"],collisionMatrix:[[true],[false,false,true],[false,true,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/未命名.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",resources:"af272",main:"20e83"}};
